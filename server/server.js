5506a4dcd0ef925f884f5ce95a78fcd2e84e7c0a
Disha2022 <99584117+Disha2022@users.noreply.github.com>
2022-07-20 01:22:01 -0400

    add stripe sample payment, cors, /secret

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const cors = require('cors');
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc')
+
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +9,17 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors())
+
+app.get('/secret', async (req, res) => {
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 1099,
+    currency: 'usd',
+    payment_method_types: ['card'],
+  });
+  res.json({client_secret: paymentIntent.client_secret});
+});
+
 
 db.once('open', () => {
   app.listen(PORT, () => {
