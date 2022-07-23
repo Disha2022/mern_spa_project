d10b07c465a2b9f838c72e8a207e9801e316c309
disha <mumudb@gmail.com>
2022-07-23 14:54:58 -0400

    add apollo server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,33 @@
 const express = require('express');
+const {ApolloServer} = require('apollo-server-express');
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
 const cors = require('cors');
 const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc')
 
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors())
+
+// Uncomment if need to serve up static assets
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 app.get('/secret', async (req, res) => {
   const paymentIntent = await stripe.paymentIntents.create({
@@ -20,9 +38,18 @@ app.get('/secret', async (req, res) => {
   res.json({client_secret: paymentIntent.client_secret});
 });
 
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-  });
-});
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+  };
+  
+  // Call the async function to start the server
+  startApolloServer(typeDefs, resolvers);
