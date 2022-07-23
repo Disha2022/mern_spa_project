656166703cd4dfc2dd39a220d3d14b3422b90dc3
Dean <deank242@gmail.com>
2022-07-23 17:34:20 -0400

    added to profile structure for workouts

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 
+import WorkoutList from '../components/'
+
 // import { useQuery, useMutation } from '@apollo/client';
 
 import Auth from '../utils/auth';
@@ -36,12 +38,20 @@ const Profile = (props) => {
                 <h2 className="bg-dark text-secondary p-3 display-inline-block">
                     Viewing {userParam ? `${user.username}'s` : 'your'} profile.
                 </h2>
+
+                {userParam && (
+                    <button className='btn ml-auto' onClick={handleClick}>
+                        Add a Workout
+                    </button>
+                )}
             </div>
 
             <div className="flex-row justify-space-between mb-3">
                 <div className="col-12 mb-3 col-lg-8">
-                </div>
-                <div className='col-12 col-lg-3 mb-3'>
+                    <WorkoutList
+                        workouts={user.thoughts}
+                        title={`${user.username}'s workouts...`}
+                    />
                 </div>
             </div>
         </div>
