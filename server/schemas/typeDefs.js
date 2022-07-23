8b0a90bc1355e776f7f4ebbff07e35dfbdb4fefa
disha <mumudb@gmail.com>
2022-07-23 14:56:52 -0400

    add typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    workouts: [Workout]
+  }
+
+  type Workout {
+    _id: ID
+    workoutText: String
+    createdAt: String
+    username: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    workouts(username: String): [Workout]
+    workout(_id: ID!): Workout
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addWorkout(workoutText: String!): Workout
+  }
+`;
+
+module.exports = typeDefs;
