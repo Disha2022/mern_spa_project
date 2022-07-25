d2e6506b47e97f91d106d0a73f734fa6f4d1c2f6
disha <mumudb@gmail.com>
2022-07-24 21:24:08 -0400

    add get to workout day

--- server/models/Workout.js
+++ server/models/Workout.js
@@ -5,7 +5,7 @@ const WorkoutSchema = new Schema({
     day: {
         type: Date,
         default: Date.now,
-        unique: true
+        get: timestamp => dateFormat(timestamp)
     },
     username: {
         type: String,
