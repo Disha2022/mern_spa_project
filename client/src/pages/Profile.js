c2dc2f2bb3d99021db69f64cead4260639d612ca
disha <mumudb@gmail.com>
2022-07-26 02:01:33 -0400

    add workoutlist/form placeholder

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,6 +4,8 @@ import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth'
+import WorkoutList from '../components/WorkoutList';
+import WorkoutForm from '../components/WorkoutForm';
 
 const Profile = (props) => {
     const { username: userParam } = useParams();
@@ -32,32 +34,26 @@ const Profile = (props) => {
         );
     }
 
-    function handleClick() {
-
-    }
-
     return (
         <div>
             <div className="flex-row mb-3">
                 <h2 className="bg-dark text-secondary p-3 display-inline-block">
                     Viewing {userParam ? `${user.username}'s` : 'your'} profile.
                 </h2>
-
-                {userParam && (
-
-                    <button className='btn ml-auto' onClick={handleClick}>
-                        Add a Workout
-                    </button>
-                )}
+                {user.username && (
+                    <WorkoutForm></WorkoutForm>
+                )
+                }
             </div>
 
             <div className="flex-row justify-space-between mb-3">
                 <div className="col-12 mb-3 col-lg-8">
-
-                    {/* <WorkoutList
-                        workouts={user.workouts}
-                        title={`${user.username}'s workouts...`}
-                    /> */}
+                    {userParam && (
+                        <WorkoutList
+                            workouts={user.workouts}
+                            title={`${user.username}'s workouts...`}
+                        />
+                    )}
                 </div>
             </div>
         </div>
